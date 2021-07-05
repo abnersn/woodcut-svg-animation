d63c6bb396054c903ace3c4a787514d4527188ab
Abner <abnersousanascimento@gmail.com>
2021-07-05 13:21:39 -0300

    Add gsap

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,11 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
 export default {
     input: 'src/index.js',
     output: {
         file: 'public/bundle.js'
-    }
+    },
+    plugins: [
+        nodeResolve()
+    ]
 };
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import gsap from "gsap";
+
+gsap.to('.sun', {
+    duration: 10,
+    transform: 'rotate3d(0, 0, 1, 300deg)'
+})
