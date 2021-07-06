eb3745fcf555262c9fa7e4be4914f52f917b6622
Abner <abnersousanascimento@gmail.com>
2021-07-06 12:41:43 -0300

    Run and add pretier

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
-import gsap from "gsap";
-import { Cubic } from "gsap/all";
-import { Sine } from "gsap/gsap-core";
-import { Linear } from "gsap/gsap-core";
-import { Power2 } from "gsap/gsap-core";
-import { Circ } from "gsap/gsap-core";
-import ScrollTrigger from "gsap/ScrollTrigger";
+import gsap from 'gsap'
+import { Cubic } from 'gsap/all'
+import { Sine } from 'gsap/gsap-core'
+import { Linear } from 'gsap/gsap-core'
+import { Power2 } from 'gsap/gsap-core'
+import { Circ } from 'gsap/gsap-core'
+import ScrollTrigger from 'gsap/ScrollTrigger'
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger)
 
 async function fetchSVG() {
-    const $objects = Array.from(document.getElementsByTagName('object'));
+    const $objects = Array.from(document.getElementsByTagName('object'))
     for (const $object of $objects) {
-        const src = $object.getAttribute('data');
-        const className = $object.getAttribute('class');
-        const style = $object.getAttribute('style');
+        const src = $object.getAttribute('data')
+        const className = $object.getAttribute('class')
+        const style = $object.getAttribute('style')
 
-        const svgContent = await fetch(src).then(res => res.text());
-        
-        $object.insertAdjacentHTML('afterend', svgContent);
-        const $svg = $object.nextElementSibling;
-        $svg.removeAttribute('id');
-        $svg.style = style;
-        $svg.classList.add(className);
+        const svgContent = await fetch(src).then((res) => res.text())
 
-        $object.remove();
+        $object.insertAdjacentHTML('afterend', svgContent)
+        const $svg = $object.nextElementSibling
+        $svg.removeAttribute('id')
+        $svg.style = style
+        $svg.classList.add(className)
+
+        $object.remove()
     }
 }
 
@@ -34,41 +34,57 @@ const paperColors = [
     'white',
     '#ffc78d',
     '#cfd2d3',
-    '#afdff8'
-];
+    '#afdff8',
+]
 
-const paperColor = paperColors[Math.random() * paperColors.length >> 0];
-document.getElementById('paper').style
-    .setProperty('--paper-color', paperColor);
+const paperColor = paperColors[(Math.random() * paperColors.length) >> 0]
+document.getElementById('paper').style.setProperty('--paper-color', paperColor)
 
 async function animate() {
-    await fetchSVG();
+    await fetchSVG()
 
     const skyTimeline = gsap.timeline({
         scrollTrigger: {
-            trigger: ".container",
+            trigger: '.container',
             scroller: '.scroller',
             start: 'top left',
             horizontal: true,
-            end: "right right",
-            scrub: true
-        }
-    });
+            end: 'right right',
+            scrub: true,
+        },
+    })
 
     skyTimeline
         .to('.sun', { ease: Circ.easeIn, translateY: '200%' })
         .to('.sun', { ease: Linear.easeOut, translateX: '-100vw' }, '<')
         .to('.night', { ease: Circ.easeOut, translateY: '-20%' }, '-=30%')
-        .to('.night', { ease: Linear.easeOut, translateX: '-50vw' }, '<');
-    
+        .to('.night', { ease: Linear.easeOut, translateX: '-50vw' }, '<')
+
     const cowTimeline = gsap.timeline({
-        repeat: -1
-    });
+        repeat: -1,
+    })
 
     cowTimeline
         .addLabel('start')
-        .to('.cow #tail', { duration: 1, ease: Sine.easeInOut, rotate: -10, transformOrigin: 'right top', yoyo: true, repeat: 7 })
-        .to('.cow #head', { ease: Sine.easeInOut, rotate: 10, duration: 2, yoyo: true, repeat: 3 }, 'start += 0.5')
+        .to('.cow #tail', {
+            duration: 1,
+            ease: Sine.easeInOut,
+            rotate: -10,
+            transformOrigin: 'right top',
+            yoyo: true,
+            repeat: 7,
+        })
+        .to(
+            '.cow #head',
+            {
+                ease: Sine.easeInOut,
+                rotate: 10,
+                duration: 2,
+                yoyo: true,
+                repeat: 3,
+            },
+            'start += 0.5'
+        )
 }
 
-window.addEventListener('load', animate);
+window.addEventListener('load', animate)
